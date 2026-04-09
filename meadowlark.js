24b711454a99c93457248ab4685ce30595b3a420
Saitejaguddeti <saitejaguddeti@Sais-MacBook-Air.local>
2026-04-09 15:28:07 -0400

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,13 +1,7 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 var app = express();
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-    ];
 
 app.use(express.static(__dirname + '/public'));
 // set up handlebars view engine
@@ -27,10 +21,8 @@ app.get('/', function(req, res) {
     res.render('about');
     });*/
 
-    app.get('/about', function(req, res){
-        var randomFortune =
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-        res.render('about', { fortune: randomFortune });
+    app.get('/about', function(req, res) {
+        res.render('about', { fortune: fortune.getFortune() } );
         });
     // 404 catch-all handler (middleware)
     app.use(function(req, res, next){
